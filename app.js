6aa319eb67b71d38466a99b86b035edb7b175680
Adli chalbi <adlichalbi6612@gmail.com>
2024-07-14 10:31:50 +0100

    added copy to clipboard feature

--- app.js
+++ app.js
@@ -110,3 +110,44 @@ const generateNewPasswords = () => {
 };
 
 btn.addEventListener("click", generateNewPasswords);
+
+const showSnackBar = () => {
+  let sb = document.getElementById("snackbar");
+  sb.className = "show";
+  setTimeout(() => {
+    sb.className = sb.className.replace("show", "");
+  }, 3000);
+};
+
+const copyText = (passId) => {
+  const passElement = document.getElementById(passId);
+
+  if (passElement) {
+    const range = document.createRange();
+    //select the text node of the element
+    range.selectNodeContents(passElement);
+    // get the selection object
+    const selection = window.getSelection();
+    //remove any existing selections
+    selection.removeAllRanges();
+    //add the new range to the selection
+    selection.addRange(range);
+
+    //Copy the selected text to the clipboard
+    navigator.clipboard
+      .writeText(selection.toString())
+      .then(() => {
+        //show success message
+        showSnackBar();
+      })
+      .catch((err) => {
+        //show error message
+        alert("Failed to copy the password. Please try again.");
+        console.error("Error copying text : ", err);
+      });
+    //clear the selection
+    selection.removeAllRanges();
+  } else {
+    console.error(`Element with id "${passId}" does not exist.`);
+  }
+};
